6291758b910b81f12b96f85c70deaf012d98f260
Roland Mungure <mungureroland@gmail.com>
2024-02-17 22:11:05 +0200

    first commit

---
+++ StoreManagement/StoreManagement/Form3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StoreManagement
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            this.Hide();
+            f1.ShowDialog();
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("Products.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine($"{tbName.Text};{tbProducer.Text};{numericUpDown1.Value}");
+
+            tbName.Clear();
+            tbProducer.Clear();
+            numericUpDown1.Value = 0;
+
+
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
